172fc2d92a599a9d0c692f3e8ac507de7e5f7447
Yashasvi <yashasvisharma846@gmail.com>
2024-12-31 21:20:08 +0530

    Initial commit

---
+++ slider.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
+
+  
